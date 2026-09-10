4067d9ea4cd096abb581b9940bf6cf110f1b936c
arch-sentinel <jaafarelfahel@gmail.com>
2026-09-10 23:48:53 +0300

    Added Web Api using ASP.NET

---
+++ PocketLedger-API/Migrations/20260910194110_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PocketLedger_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    category = table.Column<string>(type: "TEXT", nullable: true),
+                    amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    e_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+        }
+    }
+}
--- PocketLedger-API/Program.cs
+++ PocketLedger-API/Program.cs
@@ -1,5 +1,23 @@
 ﻿using PocketLedger;
+using PocketLedger.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Main_menu loader = new ();
+var builder = WebApplication.CreateBuilder(args);
 
-loader!.Load_interactive_menu();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Pocket_database>();
+
+var Pocket_web_app = builder.Build();
+
+Pocket_web_app.UseSwagger();
+Pocket_web_app.UseSwaggerUI();
+
+Pocket_web_app.UseRouting();
+Pocket_web_app.MapControllers();
+
+Pocket_web_app.Run();
